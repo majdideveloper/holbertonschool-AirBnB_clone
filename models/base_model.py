2a1a0764253e3cdbb2a2192fd6dc86d6a4bfbca5
majdi aribi <majdi.developer@gmail.com>
2022-10-10 11:44:26 +0100

    test Base model

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,6 @@ class BaseModel():
         that updat the piblic instance
         """
         self.updated_at = datetime.now()
-        return str(self.updated_at)
 
     def to_dict(self):
         """
@@ -39,7 +38,7 @@ class BaseModel():
         """
         
         self.__dict__['__class__'] = self.__class__.__name__
-        self.__dict__['update_at'] = self.updated_at.isoformat()
+        self.__dict__['updated_at'] = str(self.updated_at.isoformat())
         self.__dict__['id'] = str(uuid.uuid4())
         self.__dict__['created_at'] = self.created_at.isoformat()
 

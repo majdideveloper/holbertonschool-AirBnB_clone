400f9d6a446f8aacb4edafd8fa484ab3e9cad3a3
majdi aribi <majdi.developer@gmail.com>
2022-10-10 11:33:29 +0100

    test Base model

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,14 @@ class BaseModel():
     Class BaseModel that define all commun 
     attribute methods for other class
     """
-
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     
+
+   
+
     def __str__(self):
         """
         print
@@ -36,7 +39,7 @@ class BaseModel():
         
         self.__dict__['__class__'] = self.__class__.__name__
         self.__dict__['update_at'] = self.update_at.isoformat()
-        self.__dict__['id'] = self.id
+        self.__dict__['id'] = str(uuid.uuid4())
         self.__dict__['created_at'] = self.created_at.isoformat()
 
         return self.__dict__

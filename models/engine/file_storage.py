610354f608820d456c04bca9fd969fbfed798100
majdi aribi <majdi.developer@gmail.com>
2022-10-11 10:51:51 +0100

    :construction:first test for Store first Object:construction:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,34 +14,11 @@ class FileStorage:
     and deserializes JSON file to instances:
     """
 
-    """ def __init__(self, file_path="", objects=None):
-        self.file_path = file_path
-       self.objects = {}
-    """
+    
     __file_path = "file.json"
     __objects = {}
 
-    """ 
-    @property
-    def file_path(self):
-        return self.__file_path
-
-    @file_path.setter
-    def file_path(self, value):
-        if not isinstance(value, str):
-            raise TypeError("file_path must be an String")
-        self.__file_path = value
-
-    @property
-    def objects(self):
-        return self.__objects
-
-    @objects.setter
-    def objects(self, value):
-        if not isinstance(value, dict):
-            raise TypeError("objects must be an dict")
-        self.__objects = value
-    """    
+    
 
     def all(self):
         """
@@ -63,29 +40,21 @@ class FileStorage:
         """
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "w") as f:
+        #  if os.path.exists(FileStorage.__file_path):
+        with open(FileStorage.__file_path, "w") as f:
                 json.dump(objdict, f)
 
-    """
+   
     def reload(self):
-
-       public instance that deserializes the json file
-
-        file_name = self.__file_path
-        if os.path.exists(file_name):
-            with open(file_name, "w") as jsonFile:
-                self.__objects = json.load(file_name)
-    """
-    def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """
+        deserializes the JSON file to __objects
+        """
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
-
+            with open(self.__file_path, "r", encoding="UTF-8") as f:
+                obj = json.load(f)
+            for k, v in obj.items():
+                class_name = k.split('.')[0]
+                self.__objects[k] = eval(class_name)(**v)
+        except BaseException:
+            pass
+    

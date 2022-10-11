f9fd8840ee481ef89963e8ae236609d26235f75c
majdi aribi <majdi.developer@gmail.com>
2022-10-11 09:50:11 +0100

    :construction:first test for Store first Object:construction:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,6 @@ HBNB CLONE
 
 
 from models.base_model import BaseModel
-#from models.__init__ import storage
 import json
 import os.path
 
@@ -15,10 +14,14 @@ class FileStorage:
     and deserializes JSON file to instances:
     """
 
-    def __init__(self, file_path="", objects=None):
+    """ def __init__(self, file_path="", objects=None):
         self.file_path = file_path
-        self.objects = {}
+       self.objects = {}
+    """
+    __file_path = "file.json"
+    __objects = {}
 
+    """ 
     @property
     def file_path(self):
         return self.__file_path
@@ -38,6 +41,7 @@ class FileStorage:
         if not isinstance(value, dict):
             raise TypeError("objects must be an dict")
         self.__objects = value
+    """    
 
     def all(self):
         """
@@ -57,19 +61,31 @@ class FileStorage:
         public instance that serializes
         objects to the json file
         """
-        file_name = self.__file_path
-        if os.path.exists(file_name):
-            with open(file_name, "w") as jsonFile:
-                json.dump(self.__objects, file_name)
-   #     storage.save()
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "w") as f:
+                json.dump(objdict, f)
 
+    """
     def reload(self):
-        """
-        public instance that deserializes the json file
-        """
+
+       public instance that deserializes the json file
+
         file_name = self.__file_path
         if os.path.exists(file_name):
             with open(file_name, "w") as jsonFile:
                 self.__objects = json.load(file_name)
-
+    """
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
 
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,7 +5,7 @@ define the unittest for the engine file
 
 import json
 import unittest
-from models.bas_model import BaseModel
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 

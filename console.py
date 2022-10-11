7553ad6bd66cb5e718c26d9738ba1de3572c4313
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-11 16:00:18 +0100

    consol create def

--- console.py
+++ console.py
@@ -3,8 +3,10 @@
 HBNB CLONE
 """
 
+from models.base_model import BaseModel 
+from models import storage
 import cmd
-
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,6 +16,7 @@ class HBNBCommand(cmd.Cmd):
     intro = ''
     prompt = '(hbnb) '
     file = None
+    classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program\n"""
@@ -28,11 +31,33 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    
+    def do_create(self, arg):
+        """
+        create  a new instnce of base model
+        """
+        args = arg.split(" ")
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            new_instance = eval("{}{}".format(args[0]))
+            new_instance.save()
+            print(new_instance.id)
+
+
+
+
     # ----- record and playback -----
     def close(self):
         if self.file:
             self.file.close()
             self.file = None
+
+
 
 
 if __name__ == '__main__':

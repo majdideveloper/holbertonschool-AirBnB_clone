2c0948b2f43ff34e1d00dc14c4f7942454ccad34
majdi aribi <majdi.developer@gmail.com>
2022-10-12 15:21:48 +0100

    test user email

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+hbnb clone
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity"""
+    name = ""
+
---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+hbnb clone
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class City"""
+    state_id = ""
+    name = ""
+

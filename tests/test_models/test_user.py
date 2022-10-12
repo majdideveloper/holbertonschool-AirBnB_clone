4a8c195f2f81f560e24d096d8e57c72d0045eea5
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-12 14:03:38 +0100

    🚧add test file User🚧

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+uinitest for the User
+"""
+
+import unittest
+import uuid
+from datetime import datetime
+from time import sleep
+
+from models.base_model import BaseModel
+
+class Test_User(unittest.TestCase):
+    """
+    calss Test for Base Model 
+    """

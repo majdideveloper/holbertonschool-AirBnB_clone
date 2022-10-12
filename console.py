9863b146fd7e8498995f28f1b0e3fe873346ce3e
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-12 11:49:22 +0100

    🚧 update model in consol🚧

--- console.py
+++ console.py
@@ -110,7 +110,6 @@ class HBNBCommand(cmd.Cmd):
 
 
 
-
     def do_all(self, arg):
         """
         Print all string represntation of all instance based 
@@ -177,7 +176,6 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
                 
-
     # ----- record and playback -----
     def close(self):
         if self.file:
